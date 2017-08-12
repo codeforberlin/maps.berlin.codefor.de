c8b6ba576d37af6b9f35fcbe2d92c990bbb59f4b
Jochen Klar <jochenklar@gmail.com>
2017-08-12 10:42:57 +0200

    add legend

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 var _map;
+var _config;
+var _legend;
+
+function load_legend(layer_name) {
+    _legend.remove();
+
+    var layer_config = $.grep(_config.base, function(e){return e.name == layer_name; })[0];
+    if (typeof layer_config.legend != 'undefined') {
+        _legend.onAdd = function (map) {
+            var div = L.DomUtil.create('div', 'legend leaflet-control-layers leaflet-control-layers-expanded leaflet-control');
+            div.innerHTML += '<img src="' + layer_config.legend +  '">';
+            return div;
+        };
+
+        _legend.addTo(_map);
+
+        $('.legend').on('mousewheel', function(event) {
+            event.stopPropagation();
+        });
+    }
+}
 
 function init() {
     $.ajax({
@@ -6,6 +27,8 @@ function init() {
         dataType: 'json',
         success: function (config) {
             _map = L.map('map');
+            _config = config;
+            _legend = L.control({position: 'bottomleft'});
 
             var base = {};
             $.each(config.base, function(key, layer) {
@@ -17,13 +40,21 @@ function init() {
                 overlay[layer.name] = L.tileLayer(layer.url, layer.options);
             });
 
-            base[config.base[0].name].addTo(_map);
+            var layer_name = config.base[0].name;
+
+            base[layer_name].addTo(_map);
+
+            load_legend(layer_name);
 
             L.control.layers(base, overlay, {
                 collapsed: false
             }).addTo(_map);
 
             _map.setView(config.location.center, config.location.zoom);
+
+            _map.on('baselayerchange', function(layer) {
+                load_legend(layer.name);
+            })
         },
         error: function () {
             console.log('Error with json!');
